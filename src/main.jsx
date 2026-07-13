07eacf84fbddec8011c7d19be1cfc4f69d846f1d
aloft26armada-ux <aloft26armada@gmail.com>
2026-07-14 04:12:18 +0800

    main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import './styles/app.css'
+import './styles/sidebar.css'
+import './styles/chat.css'
+import './styles/input.css'
+import './styles/markdown.css'
+import './styles/settings.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
