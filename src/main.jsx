f72edf934c388a256491311a33f3c7a4db22d588
aloft26armada-ux <aloft26armada@gmail.com>
2026-07-14 05:30:36 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'highlight.js/styles/github-dark.css'
 import './index.css'
-import './styles/app.css'
-import './styles/sidebar.css'
-import './styles/chat.css'
-import './styles/input.css'
-import './styles/markdown.css'
-import './styles/settings.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
