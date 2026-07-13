9fef1195f246bd61d1eb04827c5e48f19b5c241b
aloft26armada-ux <aloft26armada@gmail.com>
2026-07-14 05:02:32 +0800

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'highlight.js/styles/github-dark.css'
 import './index.css'
 import './styles/app.css'
 import './styles/sidebar.css'
